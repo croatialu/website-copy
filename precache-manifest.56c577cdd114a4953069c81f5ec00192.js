1de8cfb47be0fd0e86215b7c08403b6e87fb28d3
CroatiaParanoia <CroatiaParanoia@users.noreply.github.com>
2020-04-25 03:20:03 +0000

    Deploying to gh-pages - 03:20:03

---
+++ precache-manifest.56c577cdd114a4953069c81f5ec00192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bd4673da9ad9e90328f60276e3e269",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dba369100deb619d9701",
+    "url": "./static/css/2.e16fe6db.chunk.css"
+  },
+  {
+    "revision": "39b3d3e3b5fe6eff1cd0",
+    "url": "./static/css/main.7a2b06ca.chunk.css"
+  },
+  {
+    "revision": "dba369100deb619d9701",
+    "url": "./static/js/2.c1b83e80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c1b83e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b3d3e3b5fe6eff1cd0",
+    "url": "./static/js/main.4e498a84.chunk.js"
+  },
+  {
+    "revision": "00693941e4e8677e5081",
+    "url": "./static/js/runtime-main.1005e53a.js"
+  }
+]);
